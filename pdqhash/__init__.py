bb67b5679cdc8346e5c8b302b632c8ae39226a19
Fausto Morales <fausto@robinbay.com>
2023-08-09 12:16:24 -0400

    Remove warning.

--- pdqhash/__init__.py
+++ pdqhash/__init__.py
@@ -1,7 +1,4 @@
 from .bindings import compute, compute_dihedral, compute_float
 import warnings
-warnings.warn(
-    "Hash vector order changed between version 0.1.8 and 0.2.0. "
-    "See https://github.com/faustomorales/pdqhash-python/issues/1 for more details.")
 from . import _version
 __version__ = _version.get_versions()['version']
